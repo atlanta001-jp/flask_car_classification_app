81a4159b14620e51b73c20a8e74e062dd543ddad
atlanta001-jp <atsushi.hamano111@gmail.com>
2024-08-21 10:59:14 +0900

    2nd commit change port

--- car_classification.py
+++ car_classification.py
@@ -51,5 +51,5 @@ def upload_file():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 8080))
+    port = int(os.environ.get('PORT', 8081))
     app.run(host ='0.0.0.0',port = port)
